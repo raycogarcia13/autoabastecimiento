3f6ccec9882534152b0c55109bc9e3a754e05841
Rayco García Fernández <rayco.garcia13@gmail.com>
2021-02-15 10:39:12 -0500

    para pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [{
+        name: 'Autoabastecimento',
+        exec_mode: 'cluster',
+        instances: 'max', // Or a number of instances
+        script: './node_modules/nuxt/bin/nuxt.js',
+        args: 'start'
+    }]
+}
